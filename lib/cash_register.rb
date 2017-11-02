d1a6cb8138e0e4e30a61f9e94ed7e11105b21913
psarma89 <priyam.sarma@gmail.com>
2017-11-01 20:59:46 -0400

    Sprinkles a few selfs in there

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -25,7 +25,7 @@ class CashRegister
       "There is no discount to apply."
     else
       self.total *= (1-(self.discount.to_f/100))
-      "After the discount, the total comes to $#{@total.to_i}."
+      "After the discount, the total comes to $#{self.total.to_i}."
     end
   end
 
